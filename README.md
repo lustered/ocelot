feb26e2abf1b4fbdd109d9ee3f4d4cf73ea31864
lustered <smellslikelavenders@gmail.com>
2021-05-12 19:48:18 -0400

    Fixed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Script meant to install a basic and decent vim environment on ocelot servers
 
-    wget -O - 'https://raw.githubusercontent.com/lustered/ocelot/master/test.sh' | bash
+    wget -O - 'https://raw.githubusercontent.com/lustered/ocelot/master/vimnstall.sh' | bash
--- vimnstall.sh
+++ vimnstall.sh
@@ -3,12 +3,17 @@ cd
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# coc settings
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/coc-vim.vim' -P ~/.vim/
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/coc-settings.json' -P ~/.vim/
+
 # Download node binary
 wget 'https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-x64.tar.xz'
 mkdir node
 tar xf node-v14.17.0-linux-x64.tar.xz -C node/ --strip-components=1
+
 # Add to path
-echo "set path = ( \$HOME/node/bin \$path )" >> .cshrc
+echo "set path = ( \$HOME/node/bin \$path )" >> ~/.cshrc
 
 # Set vim settings and install plugins
 wget 'https://raw.githubusercontent.com/lustered/ocelot/master/.vimrc' -P ~/
