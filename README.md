0a34c2c09eaf36ad118b650d74f6f251093d98be
lustered <smellslikelavenders@gmail.com>
2021-05-12 19:18:08 -0400

    Fixed up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-wget -O https://github.com/lustered/ocelot/blob/master/vimnstall.sh -P ~/
+## Script meant to install a basic and decent vim environment on ocelot servers
+
+    wget -O - 'https://raw.github.com/lustered/ocelot/blob/master/vimnstall.sh' | bash
--- vimnstall.sh
+++ vimnstall.sh
@@ -1,4 +1,5 @@
 # Install vim plug
+cd 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -7,10 +8,10 @@ wget 'https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-x64.tar.xz'
 mkdir node
 tar xf node-v14.17.0-linux-x64.tar.xz -C node/ --strip-components=1
 # Add to path
-echo "set path = ( $HOME/node/bin $path )" >> .cshrc
+echo "set path = ( \$HOME/node/bin \$path )" >> .cshrc
 
 # Set vim settings and install plugins
-wget https://github.com/lustered/ocelot/blob/master/.vimrc -P ~/
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/.vimrc' -P ~/
 vim -c "PlugInstall" \
     -c "so ~/.vimrc" \
     -c "CocInstall coc-clangd coc-sh coc-prettier coc-json coc-cmake" \
@@ -18,5 +19,5 @@ vim -c "PlugInstall" \
     -c "q"
 
 # coc settings
-wget https://github.com/lustered/ocelot/blob/master/coc-vim.vim -P ~/.vim/
-wget https://github.com/lustered/ocelot/blob/master/coc-settings.json -P ~/.vim/
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/coc-vim.vim' -P ~/.vim/
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/coc-settings.json' -P ~/.vim/
