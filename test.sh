5c6c38fdb88fb04f3ec31644d22c6ab9f4670fdd
lustered <smellslikelavenders@gmail.com>
2021-05-12 19:26:19 -0400

    Test script

--- test.sh
+++ test.sh
@@ -1,1 +1,23 @@
-echo "testing"
+# Install vim plug
+# cd 
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Download node binary
+wget 'https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-x64.tar.xz'
+mkdir node
+tar xf node-v14.17.0-linux-x64.tar.xz -C node/ --strip-components=1
+# Add to path
+echo "set path = ( \$HOME/node/bin \$path )" >> testcshrc
+
+# Set vim settings and install plugins
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/.vimrc' 
+vim -c "PlugInstall" \
+    -c "so ~/.vimrc" \
+    -c "CocInstall coc-clangd coc-sh coc-prettier coc-json coc-cmake" \
+    -c "q" \
+    -c "q"
+
+# coc settings
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/coc-vim.vim' 
+wget 'https://raw.githubusercontent.com/lustered/ocelot/master/coc-settings.json' 
